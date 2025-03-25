02e68ac28d0c7d031e9c392fba9f05e851a6dbd8
Nurahmadie <nurahmadie@gmail.com>
2025-03-26 02:47:52 +0900

    Fix in buffer read logic.

--- src/lib.zig
+++ src/lib.zig
@@ -1,15 +1,20 @@
 const std = @import("std");
 
-pub const IncludeDelimiterOption = struct {
+const BUF_SIZE: usize = 1024;
+
+pub const ReaderOption = struct {
+    bufSize: usize = BUF_SIZE,
     includeDelimiter: bool,
+    delimiter: []const u8,
 };
 
-pub fn ReaderWithDelimiter(bufsize: usize) type {
+pub fn ReaderWithDelimiter(option: ReaderOption) type {
     return struct {
         reader: std.io.AnyReader,
-        buffer: [bufsize]u8,
-        delimiter: []const u8,
+        buffer: [option.bufSize]u8 = [_]u8{0} ** option.bufSize,
+        delimiter: []const u8 = option.delimiter,
         cursor: usize = 0,
+        includeDelimiter: bool = option.includeDelimiter,
 
         pub const err = error{ EndOfStream, OutOfMemory };
 
@@ -21,14 +26,19 @@ pub fn ReaderWithDelimiter(bufsize: usize) type {
                 const linelen = std.mem.indexOf(u8, self.buffer[0..self.cursor], self.delimiter);
 
                 // We have another line at the previous excess read
-                if (linelen != null and linelen.? > 0) {
+                if (linelen != null and linelen.? >= 0) {
                     @memcpy(outbuf[0..linelen.?], self.buffer[0..linelen.?]);
 
-                    if (linelen.? < self.cursor) {
-                        @memcpy(self.buffer[0 .. self.cursor - linelen.?], self.buffer[linelen.?..self.cursor]);
-                        @memset(self.buffer[self.cursor - linelen.? .. self.buffer.len], 0);
-                        self.cursor -= linelen.?;
+                    if (linelen.? + self.delimiter.len < self.cursor) {
+                        std.mem.copyForwards(
+                            u8,
+                            self.buffer[0 .. self.cursor - (linelen.? + self.delimiter.len)],
+                            self.buffer[linelen.? + self.delimiter.len .. self.cursor],
+                        );
                     }
+
+                    @memset(self.buffer[self.cursor - (linelen.? + self.delimiter.len) .. self.buffer.len], 0);
+                    self.cursor -= linelen.? + self.delimiter.len;
 
                     return linelen.?;
                 }
@@ -96,49 +106,41 @@ pub fn ReaderWithDelimiter(bufsize: usize) type {
     };
 }
 
-pub fn readerWithDelimiter(reader: std.io.AnyReader, lineDelimiter: []const u8, comptime bufsize: usize) ReaderWithDelimiter(bufsize) {
-    return .{
-        .reader = reader,
-        .delimiter = lineDelimiter,
-        .buffer = [_]u8{0} ** bufsize,
-    };
+pub fn readerWithDelimiter(reader: std.io.AnyReader, comptime option: ReaderOption) ReaderWithDelimiter(option) {
+    return .{ .reader = reader };
 }
 
 test "ReaderWithDelimiter" {
     const testing = std.testing;
 
-    var bufferStream = std.io.fixedBufferStream("1\n22\n333\n4444");
-    var linereader: ReaderWithDelimiter(4) = readerWithDelimiter(bufferStream.reader().any(), "\n", 4);
+    var bufferStream = std.io.fixedBufferStream("1\n22\n333\n4444\n\n1\n333\n22\n");
+    var linereader = readerWithDelimiter(bufferStream.reader().any(), .{
+        .bufSize = 4,
+        .delimiter = "\n",
+        .includeDelimiter = false,
+    });
 
-    {
+    const TestCaseType = struct {
+        line: []const u8,
+        len: usize,
+    };
+
+    const testCases = [_]TestCaseType{
+        .{ .line = "1", .len = 1 },
+        .{ .line = "22", .len = 2 },
+        .{ .line = "333", .len = 3 },
+        .{ .line = "4444", .len = 4 },
+        .{ .line = "", .len = 0 },
+        .{ .line = "1", .len = 1 },
+        .{ .line = "333", .len = 3 },
+        .{ .line = "22", .len = 2 },
+    };
+
+    for (testCases) |tc| {
         var testbuffer: [4]u8 = [_]u8{0} ** 4;
         const result = try linereader.read(&testbuffer);
 
-        try testing.expectEqual(1, result);
-        try testing.expectEqualStrings("1", testbuffer[0..result]);
-    }
-
-    {
-        var testbuffer: [4]u8 = [_]u8{0} ** 4;
-        const result = try linereader.read(&testbuffer);
-
-        try testing.expectEqual(2, result);
-        try testing.expectEqualStrings("22", testbuffer[0..result]);
-    }
-
-    {
-        var testbuffer: [4]u8 = [_]u8{0} ** 4;
-        const result = try linereader.read(&testbuffer);
-
-        try testing.expectEqual(3, result);
-        try testing.expectEqualStrings("333", testbuffer[0..result]);
-    }
-
-    {
-        var testbuffer: [4]u8 = [_]u8{0} ** 4;
-        const result = try linereader.read(&testbuffer);
-
-        try testing.expectEqual(4, result);
-        try testing.expectEqualStrings("4444", testbuffer[0..result]);
+        try testing.expectEqual(tc.len, result);
+        try testing.expectEqualStrings(tc.line, testbuffer[0..result]);
     }
 }
